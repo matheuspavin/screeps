ebd5ca1ff706ba738a8151e3883f7436007388dd
matheuspavin <pavinmatheus@gmail.com>
2018-02-20 09:35:35 -0300

    Repairer

--- roles/role.repairer.js
+++ roles/role.repairer.js
@@ -3,29 +3,34 @@ var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        if(creep.memory.upgrading && creep.carry.energy == 0) {
-            creep.memory.upgrading = false;
+        if(creep.memory.repairing && creep.carry.energy == 0) {
+            creep.memory.repairing = false;
             creep.say('🔄 harvest');
 	    }
-	    if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
-	        creep.memory.upgrading = true;
+	    if(!creep.memory.repairing && creep.carry.energy == creep.carryCapacity) {
+	        creep.memory.repairing = true;
 	        creep.say('⚡ repair');
 	    }
 
-	    if(creep.memory.upgrading) {
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
-            }
-        }
-        else {
-            var closestDamagedStructure = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-            filter: (structure) => structure.hits < structure.hitsMax
+	    if(creep.memory.repairing) {
+	        var closestDamagedStructure = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+	            filter: (structure) => structure.hits < 5000 && structure.structureType == STRUCTURE_WALL
+            // filter: (structure) => structure.hits < structure.hitsMax
             });
+            
             if(closestDamagedStructure) {
                 if(creep.repair(closestDamagedStructure) == ERR_NOT_IN_RANGE)
                 creep.moveTo(closestDamagedStructure, {visualizePathStyle: {stroke: '#ffffff'}});
             }  
-           
+            
+        }
+        else {
+            var sources = creep.room.find(FIND_SOURCES);
+    	        if (sources.length){
+                    if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#e60000'}});
+				    }
+    	        }
         }
 	}
 };
