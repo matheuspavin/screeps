0a5d54e0f26a3f43859abbbed592e5fe1120d973
matheuspavin <pavinmatheus@gmail.com>
2018-02-20 23:29:39 -0300

    TowerService

--- roles/towerService.js
+++ roles/towerService.js
@@ -1,14 +1,23 @@
-var tower = Game.getObjectById('b4f259a4d6ce642615464b3e');
+var towerController =  function(tower) {
     if(tower) {
         var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
             filter: (structure) => structure.hits < structure.hitsMax
         });
-        if(closestDamagedStructure) {
+        var closestContainerDamaged = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax && structure.structureType === STRUCTURE_CONTAINER
+        });
+        if (closestContainerDamaged) {
+            tower.repair(closestContainerDamaged);
+        } else {
             tower.repair(closestDamagedStructure);
         }
-
-        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+            var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
         if(closestHostile) {
             tower.attack(closestHostile);
         }
     }
+};
+
+module.exports = {
+    towerController
+}
